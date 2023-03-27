327040ed275ac3c3a2bef7e95808caaeae23a8c8
Bruce Andrada <88586952+bmpandrada@users.noreply.github.com>
2023-03-27 15:36:25 +0800

    Add files via upload

---
+++ src/data/data.js
@@ -0,0 +1,11 @@
+import img1 from '../components/imgs/useSTate.png'
+
+
+
+export const homeData = [
+    {id:1, link:'https://react.dev/learn' }
+]
+ 
+export const lectureOne = [
+    {id:1, img:img1, text:'useState is React Hook that allows you to add state to a functional component. It returns an array with two values: the current state and a function to update it. The Hook takes an initial state value as an argument and returns an updated state value whenever the setter function is called.',}
+]
